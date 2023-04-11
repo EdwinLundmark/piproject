0e1fbfaae7e0cca20750ada7a207d98b978b8f78
Edwin Lundmark <edwin.lundmark@icloud.com>
2023-04-11 17:50:45 +0200

    Added simple static files

--- webserver.py
+++ webserver.py
@@ -5,8 +5,17 @@ import flask
 # scheduleThread = threading.Thread(target=startSchedule)
 # scheduleThread.start()
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path='/static')
+
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    return app.send_static_file('index.html')
+
+
+@app.route('/<path>')
+def sendStatic(path):
+    return app.send_static_file(path)
+
+
+app.run(debug=True)
